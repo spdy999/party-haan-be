bb4178898fa89c91eec5d7d8485ba34e0134dd62
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-02 03:32:42 +0700

    tested AppService

---
+++ src/app.service.spec.ts
@@ -0,0 +1,17 @@
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(() => {
+    service = new AppService();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should getHello', () => {
+    expect(service.getHello()).toEqual('Hello World!');
+  });
+});
