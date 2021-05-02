cc1012f3a308f3a0bfa8ee442d0ef43aa21f3786
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-02 23:48:07 +0700

    tested parties-users createPartiesUsers service

--- src/parties-users/parties-users.service.spec.ts
+++ src/parties-users/parties-users.service.spec.ts
@@ -1,6 +1,8 @@
 import { PartiesUsersService } from './parties-users.service';
 import { Repository } from 'typeorm';
 import { PartiesUsers } from './parties-users.entity';
+import { User } from 'src/users/user.entity';
+import { Parties } from 'src/parties/parties.entity';
 
 describe('PartiesUsersService', () => {
   let partiesUsersRepository: Repository<PartiesUsers>;
@@ -13,5 +15,25 @@ describe('PartiesUsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should createPartiesUsers', async () => {
+    const user = new User({ id: 1, email: 'john@m.com', password: '1234' });
+    const party = new Parties({
+      id: 1,
+      description: 'This is description',
+      imgUrl: 'http://img.jpg',
+      capacity: 5,
+    });
+    jest
+      .spyOn(partiesUsersRepository, 'create')
+      .mockImplementation(() => new PartiesUsers());
+    jest
+      .spyOn(partiesUsersRepository, 'save')
+      .mockImplementation(() =>
+        Promise.resolve(new PartiesUsers({ id: 1, user, party })),
+      );
+
+    expect(await service.createPartiesUsers(user, party)).toBeDefined();
   });
 });
