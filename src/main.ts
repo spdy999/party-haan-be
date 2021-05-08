693e858e1a3ca5ca214ae10158f448c104101de5
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-08 17:23:23 +0700

    enableCors()

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.SERVER_PORT);
 };
 
